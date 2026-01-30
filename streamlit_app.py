11a1f8a699b5140fb82ebb7d49eec046bd5ef7f1
SNATHA2 <nathanisona@gmail.com>
2026-01-30 17:38:18 -0600

    Update streamlit_app.py
    added smoothiefroot - watermelon API

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie!:cup_with_straw: ")
