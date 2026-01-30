e2fabd9fe56c4c5db81a33257759b8acfebc1832
SNATHA2 <nathanisona@gmail.com>
2026-01-30 17:24:47 -0600

    Update streamlit_app.py
    added new insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,17 @@ if ingredients_list:
 
         #st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
+my_insert_stmt = """
+INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+VALUES (?, ?)
+"""
 
-#st.write(my_insert_stmt)
-#st.stop()
 time_to_insert = st.button('Submit Order')
+
 if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!, name_on_order ', icon="✅")
+    session.sql(
+        my_insert_stmt,
+        params=[ingredients_string, name_on_order]
+    ).collect()
+
+    st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
