77b7935ae5845759f451d0c0de05f36efcf62a67
SNATHA2 <nathanisona@gmail.com>
2026-01-30 17:41:59 -0600

    Update streamlit_app.py
    added a data frame and json format to get api request in proper format

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ cnx=st.connection("snowflake")
 # New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie!:cup_with_straw: ")
